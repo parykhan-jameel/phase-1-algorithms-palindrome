cf35adb9562f9bc39871bea4a749223c873a319b
parykhan-jameel <parexan.00443431@gmail.com>
2022-10-17 19:14:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word === word.split("").reverse().join("")) {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
+
 /* 
-  Add your pseudocode here
+  We split the word and then reverse the letters and rejoin them again.
+  Then we do the check. If the orignal word is the same after reversing
+  --> the word is Palindrome (true). Otherwise it is not (false).
 */
 
 /*
   Add written explanation of your solution here
+   palindrom mean the reading of the worl will not change when you switch letter 
+  funciton that will check if it is a plaindrom or not 
 */
 
 // You can run `node index.js` to view these console logs
